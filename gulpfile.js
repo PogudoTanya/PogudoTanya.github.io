78960fccd180f9ea75a2a649d53716d1f60cbb94
Tatyana Pogudo <pogudo.tatyana@mail.ru>
2020-06-03 23:36:15 +0300

    final project

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,22 @@ var gulp = require('gulp');
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var nested = require('postcss-nested')
+var short = require('postcss-short')
 var cssnano = require('cssnano')
 
 
 gulp.task('css', function () {
     var plugins = [
         autoprefixer({overrideBrowserslist: ['last 2 versions'] }),
-        cssnano(),
+        nested,
+        short,
+      /*  cssnano(),*/
     ];
     return gulp.src('./styles/style.css')
         .pipe(postcss(plugins))
         .pipe(rename("styleout.css"))
         .pipe(gulp.dest('./styles/'));
 });
+
+
